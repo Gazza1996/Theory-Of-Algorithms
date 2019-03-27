e08b7c46d0fdba184ed5cd269e1ae7d19292a240
Gazza1996 <gkmannion@hotmail.com>
2019-03-27 16:57:35 +0000

    padfile stuff added to sha256

--- padfile.c
+++ padfile.c
@@ -66,7 +66,38 @@ int main(int argc, char *argv[]) {
     if (S == PAD1)
       M.e[0] = 0x80;
     
-  // close the file
+ nt i;
+ 17 
+   18   union msgblock M;
+ 19 
+   20   uint64_t nobits = 0;
+ 21 
+   22   uint64_t nobytes;
+ 23 
+   24   enum status S = READ;
+ 25 
+   26   // read in file, taken from input when program is ran
+   27   f = fopen(argv[1], "r");
+ 28 
+   29   // not reached end of file
+   30   while (S == READ) {
+     31     nobytes = fread(M.e, 1, 64, f);
+     32     // debug message
+       33     printf("Read %2llu bytes\n", nobytes);
+     34 
+       35     nobits = nobits + (nobytes * 8);
+     36     if (nobytes < 56) {
+       37       printf("Found a block with less than 55 bytes!!\n");
+       38       // adding 1
+         39       M.e[nobytes] = 0x80;
+       40       // get last 8 bytes
+         41       while (nobytes < 56) {
+           42         nobytes = nobytes + 1;
+           43         // set bytes to zero
+             44         M.e[nobytes] = 0x00;
+         47     M.s[7] = nobits;
+       48     S = FINISH;
+       49     } else if(nobytes < 64){ // close the file
   fclose(f);
 
   for(int i = 0; i < 64; i++)
--- sha256.c
+++ sha256.c
@@ -47,6 +47,9 @@ int main(int argc, char *argv[]){
   printf("\n ---- SHA256 Hashing Algorithm ----\n");
   printf("\n ----------------------------------\n");
 
+  FILE *f;
+  f = fopen(argv[1], "r");
+
  // if(argc>=1){
    // printf("\n Reading file...... \n");
    // int argCount = argc;
@@ -67,19 +70,19 @@ int main(int argc, char *argv[]){
 
 // functions
 
-char fileRead(int argCount, char *fileName){
+//char fileRead(int argCount, char *fileName){
   // vars
-  FILE *file;
-  char fileContent[10000];
-  int i;
-  uint64_t nobits = 0;
-  uint64_t nobytes;
-  enum status s = READ;
-  union msgBlock M;
+  //FILE *file;
+  //char fileContent[10000];
+  //int i;
+  //uint64_t nobits = 0;
+  //uint64_t nobytes;
+  //enum status s = READ;
+  //union msgBlock M;
 
-  file = fopen(fileName, "r");
+  //file = fopen(fileName, "r");
 
-  if(file == NULL){
+  /*if(file == NULL){
     printf("\n No file found, try again!!! \n", fileName);
   }else{
     printf("\n ---- File ----- \n");
@@ -90,9 +93,11 @@ char fileRead(int argCount, char *fileName){
       nobits = nobits + (nobytes * 8);
     }
   }
-}
+}*/
 
-void sha256(){
+void sha256(FILE *f){
+  
+  
   printf("Starting SHA256........ \n\n");
 
   // k constants. Section 4.2.2
@@ -221,3 +226,66 @@ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
   return ((x & y) ^ (x & z) ^ (y & z));
 }
+
+int nextmsgBlock{FILE *f, union msgblock *M, enum status *S, int *nobits}{
+  int i;
+
+  union msgblock M;
+
+  uint64_t nobits = 0;
+
+  uint64_t nobytes;
+
+  enum status S = READ;
+
+  // not reached end of file
+  while (S == READ) {
+    nobytes = fread(M.e, 1, 64, f);
+    // debug message
+    printf("Read %2llu bytes\n", nobytes);
+
+    nobits = nobits + (nobytes * 8);
+    if (nobytes < 56) {
+      printf("Found a block with less than 55 bytes!!\n");
+      // adding 1
+      M.e[nobytes] = 0x80;
+      // get last 8 bytes
+      while (nobytes < 56) {
+        nobytes = nobytes + 1;
+        // set bytes to zero
+        M.e[nobytes] = 0x00;
+      }
+      // set s to nobits
+    M.s[7] = nobits;
+    S = FINISH;
+    } else if(nobytes < 64){
+      S = PAD0;
+      M.e[nobytes] = 0x80;
+      while (nobytes < 64){
+        nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00;
+      }
+    } else if (feof(f)){
+      S = PAD1;
+    }
+  }
+
+  if (S == PAD0 || S == PAD1){
+    for (i = 0; i < 56; i++)
+      M.e[i] = 0x00;
+      M.s[7] = nobits;
+  }
+  if (S == PAD1)
+    M.e[0] = 0x80;
+
+    // close the file
+    fclose(f);
+
+    for (int i = 0; i < 64; i++)
+      printf("%x ", M.e[i]);
+    printf("\n");
+
+    return 0;
+  }
+
+
